9bd505d177bd7c595e711214a6549db4147fb9c7
qbiexyz <71367819+qbiexyz@users.noreply.github.com>
2024-05-07 12:03:30 +0800

    240407_1203

--- 30 practice/corr/draw_corr.R
+++ 30 practice/corr/draw_corr.R
@@ -2,11 +2,12 @@ library(readxl)
 library(tidyverse)
 library(lavaan)
 library(magrittr)
+library(ggtext)
 
 # 讀資料 ----
 ## 先預處理
 corr1 <- read_excel("30 practice/corr/corr.xlsx", 
-                   sheet = "xyz3")
+                   sheet = "xyz31")
 
 cor_nm <- colnames(corr1)[-1]
 
@@ -29,37 +30,44 @@ corr1.1 <- corr1 %>%
     yvar = factor(yvar,levels = ylevel)
   )
 
-# 畫圖 ----
+# 假設 corr1.1 已經是一個 data frame
+corr1.1 <- corr1.1 %>%
+  mutate(color_value = case_when(
+    value < 0 ~ "pa",     # 當數值小於 0 時使用這個顏色
+    value > 0 ~ "na",    # 當數值大於等於 1 時使用這個顏色
+    TRUE ~ "0"       # 其他情況不賦予顏色（可以視需求更改）
+  ))
 
+cols <- c("pa" = "#A63446", "na" = "#0C6291", "0" = "white")
+# 畫圖 ----
 p <- 
 corr1.1 %>%
-  ggplot(aes(xvar, yvar, col = value)) + ## to get the rect filled
+  ggplot(aes(xvar, yvar, col = color_value)) + 
   geom_tile(col="black", fill="white") +
   geom_point(aes(size = abs(value), fill = value), 
              shape = "circle", stroke = 0.5) +
   labs(x = NULL, y = NULL, 
        col = "",
-       title ="青少年到成人初期的睡眠問題與正/負面情緒之相關係數圖", 
-       subtitle = "睡眠問題(Sleep Problems, SP)、正面情緒(Positive Affect, PA)、負面情緒(Negative Affect, NA)\n時間範圍從國一開始(以多數13歲為代表)，包含青少年到成人初期(13y ~ 30y)。", 
-       caption = "(空白格為不顯著)，資料來源:臺灣青少年成長歷程研究國一樣本青少年到成人初期，共9波追蹤資料") +
-  theme_classic()+
-  scale_color_gradient2(mid = "#FBFEF9",low = "#A63446",high = "#0C6291", 
-                        limits = c(-1,1))  +
+       title ="不同年紀的睡眠問題與正/負面情緒之相關係數圖", 
+       subtitle = "睡眠問題(Sleep Problems, SP)、正面情緒(Positive Affect, PA)、負面情緒(Negative Affect, NA)<br>圓圈:相關係數大小，顏色:<span style='color:#A63446;'>小於0</span></b>、<span style='color:#0C6291;'>大於0</span></b>",
+       caption = "資料來源:臺灣青少年成長歷程研究國一樣本，從青春年到成人初期(13y ~ 30y)，共9波追蹤資料") +
+  theme_classic() +
+  scale_colour_manual(
+    values = cols, 
+    guide = NULL
+  ) +
   scale_x_discrete(expand = c(0,0)) +
   scale_y_discrete(expand = c(0,0)) +
-  scale_size(range = c(1,11), guide = NULL) +
+  scale_size(range = c(1,13), guide = NULL) +
   guides(fill = FALSE) + 
-  guides(colour = guide_colourbar(barwidth = 1, 
-                                  barheight = 15)) +   # 調整圖例的長度和寬度
   geom_vline(xintercept = 9.5, linetype = "solid",
              size = 1.5, color = "black") + 
-  theme(plot.title = element_text(size = 24, face = "bold"),
-        plot.subtitle = element_text(size = 12),
-        plot.caption = element_text(size = 10, color = "gray"))
+  theme(
+    plot.subtitle=element_markdown(size = 14),
+    plot.title = element_markdown(size = 24, face = "bold"),
+    plot.caption = element_markdown(size = 12, color = "gray")
+    )
 
 p
-
-
-
-
-
+ggsave("30 practice/corr/睡眠問題與正負面情緒之相關係數圖.png", 
+       plot = p, width = 10, height = 6, dpi = 300)
