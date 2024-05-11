924d9f4070e837ba554c7accfafdc09dd4f6abef
qbiexyz <71367819+qbiexyz@users.noreply.github.com>
2024-05-11 13:16:08 +0800

    240511_1316

--- 30 practice/corr/draw_corr.R
+++ 30 practice/corr/draw_corr.R
@@ -7,7 +7,7 @@ library(ggtext)
 # 讀資料 ----
 ## 先預處理
 corr1 <- read_excel("30 practice/corr/corr.xlsx", 
-                   sheet = "xyz31")
+                   sheet = "xyz3")
 
 cor_nm <- colnames(corr1)[-1]
 
@@ -16,9 +16,9 @@ xlevel <- c("PA_30y", "PA_27y", "PA_24y", "PA_22y", "PA_18y", "PA_16y",
             "PA_15y", "PA_14y", "PA_13y",
             "NA_13y", "NA_14y", "NA_15y", "NA_16y", "NA_18y", "NA_22y", 
             "NA_24y", "NA_27y", "NA_30y") 
-ylevel <- c("SP_13y", "SP_14y", "SP_15y", "SP_16y", 
-            "SP_18y", "SP_22y", "SP_24y", "SP_27y", 
-            "SP_30y")
+ylevel <- c("13y", "14y", "15y", "16y", 
+            "18y", "22y", "24y", "27y", 
+            "30y")
 
 # 整理資料 ----
 corr1.1 <- corr1 %>% 
@@ -32,38 +32,45 @@ corr1.1 <- corr1 %>%
 
 # 假設 corr1.1 已經是一個 data frame
 corr1.1 <- corr1.1 %>%
-  mutate(color_value = case_when(
+  mutate(
+    color_value = case_when(
     value < 0 ~ "pa",     # 當數值小於 0 時使用這個顏色
     value > 0 ~ "na",    # 當數值大於等於 1 時使用這個顏色
-    TRUE ~ "0"       # 其他情況不賦予顏色（可以視需求更改）
-  ))
+    TRUE ~ "0"       ), # 其他情況不賦予顏色（可以視需求更改）
+    xv = str_sub(xvar, -3, -1)
+  )
 
 cols <- c("pa" = "#A63446", "na" = "#0C6291", "0" = "white")
 # 畫圖 ----
 p <- 
 corr1.1 %>%
   ggplot(aes(xvar, yvar, col = color_value)) + 
-  geom_tile(col="black", fill="white") +
+  geom_tile(col = "black", fill = "white") +
   geom_point(aes(size = abs(value), fill = value), 
              shape = "circle", stroke = 0.5) +
-  labs(x = NULL, y = NULL, 
+  labs(x = "正面情緒 &larr; &rarr; 負面情緒",
+       y = "睡眠問題", 
        col = "",
        title ="不同年紀的睡眠問題與正/負面情緒之相關係數圖", 
-       subtitle = "睡眠問題(Sleep Problems, SP)、正面情緒(Positive Affect, PA)、負面情緒(Negative Affect, NA)<br>圓圈:相關係數大小，顏色:<span style='color:#A63446;'>小於0</span></b>、<span style='color:#0C6291;'>大於0</span></b>",
-       caption = "資料來源:臺灣青少年成長歷程研究國一樣本，從青春年到成人初期(13y ~ 30y)，共9波追蹤資料") +
+       subtitle = "圓圈大小:相關係數，顏色:相關係數<span style='color:#A63446;'>小於0</span></b>、<span style='color:#0C6291;'>大於0</span></b>",
+       caption = "資料來源:臺灣青少年成長歷程研究國一樣本，從青春期到成人初期(13y ~ 30y)，共9波追蹤資料") +
   theme_classic() +
   scale_colour_manual(
     values = cols, 
     guide = NULL
   ) +
-  scale_x_discrete(expand = c(0,0)) +
-  scale_y_discrete(expand = c(0,0)) +
-  scale_size(range = c(1,13), guide = NULL) +
+  scale_x_discrete(expand = c(0, 0), labels = corr1.1$xv) +
+  scale_y_discrete(expand = c(0, 0)) +
+  scale_size(range = c(-2, 15), guide = NULL) +
   guides(fill = FALSE) + 
   geom_vline(xintercept = 9.5, linetype = "solid",
              size = 1.5, color = "black") + 
   theme(
-    plot.subtitle=element_markdown(size = 14),
+    axis.title.x = element_markdown(size = 12, face = "bold"),
+    axis.title.y = element_markdown(size = 12, face = "bold"),
+    axis.text.x = element_markdown(size = 12),
+    axis.text.y = element_markdown(size = 12),
+    plot.subtitle = element_markdown(size = 14),
     plot.title = element_markdown(size = 24, face = "bold"),
     plot.caption = element_markdown(size = 12, color = "gray")
     )
